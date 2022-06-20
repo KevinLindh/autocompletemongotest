c91eeb22dd9f0f755e104d2b12bcdb3b31576317
Kevin Lindholm <kevin.a.lindholm@hotmail.com>
2022-06-20 22:41:18 +0300

    finish

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const {MongoClient, ObjectId } = require('mongodb')
+const { response } = require('express')
+const { request } = require('http')
 require('dotenv').config()
 const PORT = 8000
 
@@ -21,38 +23,41 @@ app.use(express.urlencoded({extended : true}))
 app.use(express.json())
 app.use(cors())
 
-app.get("/search", async (request, response)=>{
+app.get("/search", async (request,response) => {
     try {
         let result = await collection.aggregate([
             {
-                "$Search" : {
+                "$search" : {
                     "autocomplete" : {
                         "query": `${request.query.query}`,
                         "path": "title",
-                        "fuzzy" : {
-                            "maxEdits": 2,
+                        "fuzzy": {
+                            "maxEdits":2,
                             "prefixLength": 3
                         }
                     }
                 }
             }
         ]).toArray()
+        //console.log(result)
         response.send(result)
-    } catch (error){
+    } catch (error) {
         response.status(500).send({message: error.message})
+        console.log(error)
     }
 })
 
-app.get("/get/:id", async (request, response)=>{
-    try{
-        let result = await collection.find({
+app.get("/get/:id", async (request, response) => {
+    try {
+        let result = await collection.findOne({
             "_id" : ObjectId(request.params.id)
         })
         response.send(result)
-    } catch (error){
+    } catch (error) {
         response.status(500).send({message: error.message})
     }
-})
+}
+)
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server is running.`)
